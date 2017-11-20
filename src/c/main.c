28e8342ed03d262f060472d54db036054dd9c041
georg <bumzacktrara@gmail.com>
2017-11-20 17:40:03 +0100

    removed logger output

--- src/c/main.c
+++ src/c/main.c
@@ -15,10 +15,8 @@ void launch_initials_window(){
 	window_stack_push(initials_window_get_window(), true);
 }
 
-int main(){
-
+int main() {
 	cntPlayersInHighscoreList = -1;
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "'main'     cntPlayersInHighscoreList = %d   ",  cntPlayersInHighscoreList);
 
 	/* Turn on the lights */
 	light_enable(true);
@@ -32,14 +30,11 @@ int main(){
 
 	// load highscores, before a new game starts	
 	import_highscores();
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "'main'     cntPlayersInHighscoreList = %i   ",  cntPlayersInHighscoreList);
 	
 	/* Keep the app running */
 	app_event_loop();
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "'main'     cntPlayersInHighscoreList = %i   ",  cntPlayersInHighscoreList);
 
 	/* save highscores in highscore_window_load */
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "'main'     cntPlayersInHighscoreList = %i   ",  cntPlayersInHighscoreList);
 
 	/* Close windows */
 	splash_window_destroy();
