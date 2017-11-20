c209513ff4a2046dd8705027a4e8bd6700da66ff
codingClemo <clemens.kozmich@gmail.com>
2017-11-20 13:21:13 +0100

    highscores not working properly

--- src/c/highscore_window.c
+++ src/c/highscore_window.c
@@ -19,43 +19,17 @@ char highscore_list_text[100];
 void import_highscores() {
 	cntPlayersInHighscoreList = 0; 
 	cntPlayersInHighscoreList = persist_read_int(HS_CNT_PLAYERS);
-
-//<<<<<<< use_global_variable_for_highscore
+	
 	for (int i = 0; i <= cntPlayersInHighscoreList; i++) {
 		if (!persist_exists(HS_POINTS + i) || !persist_exists(HS_NAMES + i)) {
 			APP_LOG(APP_LOG_LEVEL_ERROR, "Error: there should be points/names in the storage");
 		} else {
 			highscores[i].points = persist_read_int(HS_POINTS + i);
-			int result = persist_read_data(HS_NAMES, &highscores[i].name, 4);
+			int result = persist_read_data(HS_NAMES + i, &highscores[i].name, 4);
 			if (result != 4) {
 				APP_LOG(APP_LOG_LEVEL_ERROR, "Error READING from storage");
 			}
 		}
-		
-		
-/*/=======
-void import_highscores(){
-	char buffer[4];
-	int stored_points;
-	persist_write_string(103, "du3");
-	persist_write_int(113, 7);
-	
-	for (int i = 1; i <= HIGHSCORE_LENGTH; i++){
-		// Get name 
-		if (persist_exists(100+i)) {
-			persist_read_string(100+i, buffer, sizeof(buffer));
-			names[i] = buffer;
-		} else {
-			//buffer = "ddd";
-			names[i] = "ddd";
-		}
-		//names[i] = buffer;
-		// Get points 
-		stored_points = persist_exists(110+i) ? persist_read_int(110+i) : 0;	
-		//stored_points = persist_read_int(110+i);
-		snprintf(buffer, 10, "%d", stored_points);
-		points[i] = buffer;
-//>>>>>>> master */
 	}
 
 	// TODO: REMOVE DEBUG ONLY - write data to APP LOG
@@ -76,9 +50,9 @@ void create_single_highscore_string() {
 
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "create_single_highscore_string() cntPlayersInHighscoreList = %d ", cntPlayersInHighscoreList);
 	
-	for (int i = 1; i <= cntPlayersInHighscoreList; i++) {
+	for (int i = 0; i <= cntPlayersInHighscoreList; i++) {
 		// copy name of the player into "highscore_list_text"
-		snprintf(position, sizeof(position), "%d", i);
+		snprintf(position, sizeof(position), "%d", i+1);
 		strcat(highscore_list_text, position);
 		strcat(highscore_list_text, seperator);
 		strcat(highscore_list_text, highscores[i].name);
@@ -107,31 +81,27 @@ void write_highscores(){
 
 	for (int i = 0; i <= cntPlayersInHighscoreList; i++) {
 		persist_write_int(HS_POINTS + i, highscores[i].points);
-		int result = persist_write_data(HS_NAMES, &highscores[i].name, 4);
-		if (result != 4) {
-			APP_LOG(APP_LOG_LEVEL_ERROR, "Error writing to storage");
-		}
+		persist_write_data(HS_NAMES, &highscores[i].name, 4);
 	}
 }
 
 
 
-
+/*** Click any button to exit program ***/
 static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
-//	highscore_window_unload(highscore_window);
 	highscore_window_destroy();
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-	
+	highscore_window_destroy();
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-	
+	highscore_window_destroy();
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-	
+	highscore_window_destroy();
 }
 
 static void click_config_provider(void *context) {
@@ -139,7 +109,7 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
+} 
 
 /*** Window handling ***/
 void highscore_window_load(Window *window){
@@ -154,11 +124,13 @@ void highscore_window_load(Window *window){
 	
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "highscore_window_load()  calling create_single_highscore_string()");
 	create_single_highscore_string();
-
+	write_highscores();
+	
 	highscore_text = text_layer_create(GRect(0,50,144,160));
 	text_layer_set_text(highscore_text, highscore_list_text);
 	text_layer_set_text_alignment(highscore_text, GTextAlignmentCenter);
 	layer_add_child(window_layer, text_layer_get_layer(highscore_text));
+
 }
 
 void highscore_window_unload(Window *window){
