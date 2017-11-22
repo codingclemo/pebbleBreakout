0beebb677ea473e2a5d6dd3ebeaedf76470d067a
georg <bumzacktrara@gmail.com>
2017-11-22 18:35:49 +0100

    cleanup comments

--- src/c/main.h
+++ src/c/main.h
@@ -4,13 +4,13 @@
 
 #define HIGHSCORE_LENGTH 5
 
-#define TIMEOUT_SPLASH_SCREEN 3000   // 3000
-#define TIMEOUT_INITIALS_SCREEN 3000   // 1000
-#define TIMEOUT_HIGHSCORE_SCREEN 1000  // 500
+#define TIMEOUT_SPLASH_SCREEN 3000   
+#define TIMEOUT_INITIALS_SCREEN 3000   
+#define TIMEOUT_HIGHSCORE_SCREEN 1000  
 
 typedef struct GameResult {
     char name[5];
-    int points;         // or int32_t ?? how to convert this to a string in highscore_window?
+    int points;         
 } GameResult;
 
 extern GameResult highscores[HIGHSCORE_LENGTH];
